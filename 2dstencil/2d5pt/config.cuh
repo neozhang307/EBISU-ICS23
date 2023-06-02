0c4ccc918f3c2f5600a10a138f00e4a225bd3cc5
Lingqi <zlqi@qq.com>
2023-06-02 19:29:32 +0900

    update star shape configure files

--- 2dstencil/2d5pt/config.cuh
+++ 2dstencil/2d5pt/config.cuh
@@ -1,4 +1,4 @@
-static constexpr int const &HALO = 1; // rad or halo
-static constexpr int const &FPC = 10; // flops per cell
-static constexpr int isBox = 0;       // 0: star, 1: box
-static constexpr int curshape = 1;       // 1: star, 2: box
+static constexpr int const HALO = 1;     // rad or halo
+static constexpr int const FPC = 10;     // flops per cell
+static constexpr int const isBox = 0;    // 0: star, HALO: box
+static constexpr int const curshape = 1; // 1: star, 2: box
--- 2dstencil/2ds9pt/config.cuh
+++ 2dstencil/2ds9pt/config.cuh
@@ -1,4 +1,4 @@
-static constexpr int const &HALO = 2; // rad or halo
-static constexpr int const &FPC = 18; // flops per cell
-static constexpr int isBox = 0;       // 0: star, 1: box
-static constexpr int curshape = 1;       // 1: star, 2: box
+static constexpr int const HALO = 2;     // rad or halo
+static constexpr int const FPC = 18;     // flops per cell
+static constexpr int const isBox = 0;    // 0: star, HALO: box
+static constexpr int const curshape = 1; // 1: star, 2: box
