de6078a35ac844a085829724a42e3c39d4376ce8
lingqi.zhang <lingqi.zhang@riken.jp>
2024-12-12 17:08:28 +0900

    update 3d stencil settings

--- 3dstencil/common/temporalconfig.cuh
+++ 3dstencil/common/temporalconfig.cuh
@@ -31,7 +31,8 @@ struct timesteps<halo,shape,ipt,double>
 template<>
 struct timesteps< 1,  star_shape,  4,  double>
 {
-  static int const val = 8;
+  static int const val = 3;
+  // static int const val = 8;
 };
 
 template<>
@@ -57,8 +58,8 @@ struct timesteps< 2,  star_shape,  8,  float>
 template<>
 struct timesteps<1, box_shape,  4,  double>
 {
-  static int const val = 5;
-  // static int const val = 4;
+  // static int const val = 5;
+  static int const val = 3;
   // static int const val = 1;
   // static int const val = 2;
 };
